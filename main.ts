69a0e609dcbe0c5a6f1d40127e36e07e25305d61
louna-prevert888 <103409948+louna-prevert888@users.noreply.github.com>
2022-04-13 09:25:29 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let n = 0
+while (!(input.buttonIsPressed(Button.B))) {
+    if (input.buttonIsPressed(Button.A)) {
+        n = 1
+    }
+    if (true) {
+    	
+    } else {
+        if (true) {
+        	
+        } else {
+        	
+        }
+    }
+}
+basic.forever(function () {
+	
+})
